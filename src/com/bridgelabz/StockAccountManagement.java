c5ede212941ea5b92945cdeb47365a3d8c726051
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-27 01:42:20 +0530

    Added: Welcome Message.

---
+++ src/com/bridgelabz/StockAccountManagement.java
@@ -0,0 +1,22 @@
+/*
+1. Stock Account Management
+a. Desc -> Write a program to read in Stock Names, Number of Share, Share Price.
+Print a Stock Report with the total value of each Stock and the total value of
+Stock.
+b. I/P -> N number of Stocks, for Each Stock Read In the Share Name, Number of
+Share, and Share Price
+c. Logic -> Calculate the value of each stock and the total value
+d. O/P -> Print the Stock Report.
+e. Hint -> Create Stock and Stock Portfolio Class holding the list of Stocks read
+from the input file. Have functions in the Class to calculate the value of each
+stock and the value of total stocks.
+ */
+package com.bridgelabz; // package
+
+public class StockAccountManagement {
+    //main method
+    public static void main(String[] args) {
+        //Welcome Message
+        System.out.println("Welcome to Object Oriented Practice Programs.");
+    }
+}
